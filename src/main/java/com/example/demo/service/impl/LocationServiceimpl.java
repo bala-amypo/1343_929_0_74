6b22e228f8f041e2be63aaf6e3573cc55f2774cc
bala-amypo <bala@amypo.in>
2025-12-15 16:51:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/LocationServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/LocationServiceimpl.java
@@ -1,10 +1,17 @@
 package com.example.demo.service.impl;
 
+import org.hibernate.mapping.List;
 import org.springframework.stereotype.Service;
 
+import com.example.demo.entity.Location;
 import com.example.demo.service.LocationService;
 
 @Service
 public class LocationServiceimpl  implements LocationService{
-    
+    public Location createLocation(Location location){
+
+    }
+    public List<Location> getAllLocation(){
+
+    }
 }
