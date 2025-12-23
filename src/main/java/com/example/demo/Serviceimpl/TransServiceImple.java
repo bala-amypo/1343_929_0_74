d81bdb065f522ac0cb2e2f1a89443a7ae2ce0c54
bala-amypo <bala@amypo.in>
2025-12-23 04:59:55 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/TransServiceImple.java
+++ src/main/java/com/example/demo/Serviceimpl/TransServiceImple.java
@@ -2,6 +2,8 @@ package com.example.demo.Serviceimpl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.example.demo.entity.Trans;
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.TransRepo;
 
 public class TransServiceImple {
@@ -9,6 +11,7 @@ public class TransServiceImple {
     TransRepo transRepo;
 
     public Trans saveTrans(Trans trans){
-        transRepo.
+        transRepo.save(trans);
+        throw new ResourceNotFoundException("Testing trans");
     }
 }
--- src/main/java/com/example/demo/repository/TransRepo.java
+++ src/main/java/com/example/demo/repository/TransRepo.java
@@ -3,6 +3,6 @@ package com.example.demo.repository;
 import com.example.demo.entity.Trans;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class TransRepo extends JpaRepository<Trans, Long> {
+public interface TransRepo extends JpaRepository<Trans, Long> {
     
 }
